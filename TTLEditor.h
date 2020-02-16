869f2c14f2f8da5b5c76e01561489b55426b6e59
root <root@localhost>
2020-02-16 14:37:55 +0400

    new

---
+++ TTLEditor.h
@@ -0,0 +1,49 @@
+#ifndef TTLEDITOR_H
+#define TTLEDITOR_H
+
+#include <QWidget>
+
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTextEdit>
+
+#include <QProcess>
+
+class QGridLayout;
+
+class QLineEdit;
+class QPushButton;
+class QTextEdit;
+
+class QProcess;
+
+class TTLEditor : QWidget
+{
+	Q_OBJECT
+	
+	private:
+	
+	QGridLayout *mainLayout;
+	
+	QPushButton *btnCurrent;
+	QPushButton *btnSet;
+	QPushButton *btnRestore;
+	
+	QLineEdit *numTTL;
+	
+	QTextEdit *logText;
+	
+	QProcess *procTTL;
+	
+	public:
+	TTLEditor(QWidget *parent = nullptr);
+	~TTLEditor();
+	
+	private slots:
+	void setTTL();
+	void currentTTL();
+	void restoreTTL();
+}
+
+#endif TTLEDITOR_H
